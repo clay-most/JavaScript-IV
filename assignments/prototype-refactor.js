87dae0dc269d90b918f7df3d6c66f999577c5268
clay-most <46730958+clay-most@users.noreply.github.com>
2019-04-25 16:40:36 -0600

    refactor prototype game

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,85 @@
-/* 
+class GameObject {
+  constructor(player) {
+    this.createdAt = player.createdAt;
+    this.dimensions = player.dimensions;
+    this.name = player.name;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
 
-Prototype Refactor
+class CharacterStats extends GameObject {
+  constructor(player) {
+    super(player);
+    this.healthPoints = player.healthPoints;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
 
-1. Copy and paste your code or the solution from yesterday
+class Humanoid extends CharacterStats {
+  constructor(player) {
+    super(player);
+    this.team = player.team;
+    this.weapons = player.weapons;
+    this.language = player.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Toungue"
+});
 
-*/
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+console.log(mage.createdAt);
+console.log(archer.dimensions);
+console.log(swordsman.healthPoints);
+console.log(mage.name);
+console.log(swordsman.team);
+console.log(mage.weapons);
+console.log(archer.language);
+console.log(archer.greet());
+console.log(mage.takeDamage());
+console.log(swordsman.destroy());
